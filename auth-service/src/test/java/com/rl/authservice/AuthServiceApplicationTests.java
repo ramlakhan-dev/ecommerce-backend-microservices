d75bea78b1af1c3d74af1089851cb9cbe6802a95
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2026-02-05 21:00:44 +0530

    initial commit

---
+++ auth-service/src/test/java/com/rl/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rl.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
